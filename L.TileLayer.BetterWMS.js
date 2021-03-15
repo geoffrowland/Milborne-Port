59cc86ed7b11f61c37a353d624682969a473fb6b
Geoff Rowland <rowland.geoff@gmail.com>
2021-03-15 22:23:06 +0000

    Update L.TileLayer.BetterWMS.js

--- L.TileLayer.BetterWMS.js
+++ L.TileLayer.BetterWMS.js
@@ -151,7 +151,7 @@ L.TileLayer.BetterWMS = L.TileLayer.WMS.extend({
 		blseon2 = blseon2.split("\'")[0];
 		if (blseon1 == blseon2) {blseon = blseon1} else {blseon = blseon1 + ' to ' + blseon2};
 		if (blseon != '') {blseon = '<br/>Eon: ' + blseon};
-	    blscontent = blsname + blsdesc + blstime + blsage + blsepoch + blssubperiod + blsperiod + blsera + blseon + '<br/><br/>Locaton: ' + bngString + '<br/>Lat Lng: ';
+	    blscontent = blsname + blsdesc + blstime + blsage + blsepoch + blssubperiod + blsperiod + blsera + blseon + '<br/><br/>Locaton: ' + bngString + '<br/>';
     };
 
     slscontent = '';
